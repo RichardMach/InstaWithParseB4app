7f226ec9f8cd9efe92ad150a43313e5bb57bafaa
richard <richardmachado.93@gmail.com>
2019-07-11 11:13:20 -0300

    Layout Cadastro

---
+++ app/src/main/java/co/nf/axesoft/instawithparseb4app/activity/CadastroActivity.java
@@ -0,0 +1,16 @@
+package co.nf.axesoft.instawithparseb4app.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import co.nf.axesoft.instawithparseb4app.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
