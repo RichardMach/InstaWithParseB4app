889bf151cf77c4378a26e4253b6c851cb98da783
richard <richardmachado.93@gmail.com>
2019-07-11 12:20:11 -0300

    Layout Cadastro

--- app/src/main/java/co/nf/axesoft/instawithparseb4app/activity/CadastroActivity.java
+++ app/src/main/java/co/nf/axesoft/instawithparseb4app/activity/CadastroActivity.java
@@ -2,15 +2,35 @@ package co.nf.axesoft.instawithparseb4app.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import co.nf.axesoft.instawithparseb4app.R;
 
 public class CadastroActivity extends AppCompatActivity {
 
+    private TextView txtAbrirLogin;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        txtAbrirLogin = findViewById(R.id.txtAbrirLogin);
+        txtAbrirLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirLogin();
+                finish();
+            }
+        });
+
+
+    }
+    public void abrirLogin(){
+        startActivity( new Intent(CadastroActivity.this,LoginActivity.class));
     }
 }
--- app/src/main/java/co/nf/axesoft/instawithparseb4app/activity/LoginActivity.java
+++ app/src/main/java/co/nf/axesoft/instawithparseb4app/activity/LoginActivity.java
@@ -2,15 +2,41 @@ package co.nf.axesoft.instawithparseb4app.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import co.nf.axesoft.instawithparseb4app.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView txtAbrirCadastro;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtAbrirCadastro = findViewById(R.id.txtCadastrar);
+        txtAbrirCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirCadastro();
+            }
+        });
+
+
+
+
+
+
+
+
+    }
+    public void abrirCadastro(){
+
+        startActivity( new Intent(LoginActivity.this,CadastroActivity.class));
+
     }
 }
